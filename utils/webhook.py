ea50649533358e6cce372a8480909e9f35b8060f
Tibabalase Oludemi <tibabalase@gmail.com>
2022-01-04 07:04:18 -0500

    Update webhook.py

--- utils/webhook.py
+++ utils/webhook.py
@@ -29,8 +29,8 @@ async def send_webhook(_dict, url, webhook_client, message='NO_REPEAT'):
         color = Colour.dark_orange()
 
     embed = Embed(title=product_name, color=color, url=link)
-    embed.set_footer(text='WINX4 Bots - winwinwinwin#0001',
-                     icon_url='https://images6.alphacoders.com/909/thumb-1920-909641.png')
+    embed.set_footer(text='Cookbeast | Footlocker.ae',
+                     icon_url='https://i.imgur.com/qrDSagP.jpg')
     embed.set_thumbnail(url=image_link)
     embed.add_field(name='Status', value=title, inline=False)
     embed.add_field(name='Price', value=f'{price} AED', inline=False)
@@ -47,6 +47,31 @@ async def send_webhook(_dict, url, webhook_client, message='NO_REPEAT'):
                                )
             logger().info('Sent Webhook')
             break
+        except Exception:
+            logger().exception('Webhook Failed')
+            await sleep(2)
+
+
+async def send_err_webhook():
+    embed = Embed(title='Error', color=Colour.red())
+    embed.add_field(name='Status', value='Fuck.')
+    embed.set_thumbnail(url='https://i.gyazo.com/98e5097c2adccb00a43c89b6038c94ee.png')
+
+    for _ in range(3):
+        try:
+            async with aiohttp.ClientSession() as webhook_client:
+                webhook = Webhook.from_url(
+                    url='https://discord.com/api/webhooks/927893447808008273/'
+                        'UlFfNapvHqzB2z4D_D_RPXgXRmRufwOcKTO3zZ6UaI8T-_mPQQx4UbaaIy6xTgIN_-eY',
+                    adapter=AsyncWebhookAdapter(webhook_client))
+                await webhook.send(username='FTL.AE Monitor - Early#0001',
+                                   avatar_url=
+                                   'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/'
+                                   '0002/5414/brand.gif?itok=c0LjV97s',
+                                   embed=embed,
+                                   )
+                logger().info('Sent Error Webhook to #winx4')
+            return
         except Exception:
             logger().exception('Webhook Failed')
             await sleep(2)
